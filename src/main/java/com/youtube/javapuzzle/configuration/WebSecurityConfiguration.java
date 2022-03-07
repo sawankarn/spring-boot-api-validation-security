0debd93d6f90b1308a89672b8469118a3ad34bc1
Sawan Karn <78696820+sawankarn@users.noreply.github.com>
2022-03-07 16:25:44 +0530

    Add files via upload

---
+++ src/main/java/com/youtube/javapuzzle/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,55 @@
+package com.youtube.javapuzzle.configuration;
+
+import com.youtube.javapuzzle.service.MyCustomUserDetailsService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return new MyCustomUserDetailsService();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService());
+        daoAuthenticationProvider.setPasswordEncoder(bCryptPasswordEncoder());
+        return daoAuthenticationProvider;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authenticationProvider());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests()
+//                .anyRequest().permitAll()
+//                .antMatchers(HttpMethod.POST, "/api/users").hasAuthority("ADMIN")
+//                .antMatchers(HttpMethod.GET, "/api/users").hasAnyAuthority("ADMIN", "EDITOR")
+                .anyRequest().authenticated()
+                .and().formLogin()
+                .and().httpBasic();
+    }
+}
---
+++ src/main/java/com/youtube/javapuzzle/service/MyCustomUserDetailsService.java
@@ -0,0 +1,21 @@
+package com.youtube.javapuzzle.service;
+
+import com.youtube.javapuzzle.entity.User;
+import com.youtube.javapuzzle.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class MyCustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if(user == null) throw new UsernameNotFoundException("Could not able to find the user.");
+        return new MyCustomUserDetails(user);
+    }
+}
